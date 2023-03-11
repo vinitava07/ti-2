3b4b7c0c060ef616e70aead63a45058c3bbe62e6
AlexandreCJ <acjurka@sga.pucmias.br>
2023-03-11 18:18:39 -0300

    creating powerChatHttpClient

--- gpt/src/main/java/com/powerchat/gpt/PowerChatHttpClient.java
+++ gpt/src/main/java/com/powerchat/gpt/PowerChatHttpClient.java
@@ -1,34 +1,24 @@
 package com.powerchat.gpt;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpClient.Redirect;
-import java.net.http.HttpClient.Version;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
 
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-
-@SpringBootTest
-class GptApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-	@Test
-	void requestChatGPT() throws IOException, InterruptedException {
-		HttpRequest request = HttpRequest.newBuilder()
+public class PowerChatHttpClient {
+	private HttpClient client;
+	private HttpRequest request;
+	
+	PowerChatHttpClient() {
+		request = HttpRequest.newBuilder()
 		         .uri(URI.create("http://foo.com/"))
 		         .build();
-		HttpClient client = HttpClient.newBuilder()
+		client = HttpClient.newBuilder()
 		        .version(Version.HTTP_1_1)
 		        .followRedirects(Redirect.NORMAL)
 		        .connectTimeout(Duration.ofSeconds(20))
@@ -37,5 +27,6 @@ class GptApplicationTests {
 		   System.out.println(response.statusCode());
 		   System.out.println(response.body());
 	}
-
+	
+	
 }
