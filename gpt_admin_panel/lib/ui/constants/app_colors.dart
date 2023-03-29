452ba7c72073bc80f1131c9c422c33b136fa80a3
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2023-03-28 22:47:48 -0300

    Creating LoginScreen and Dashboard navigation Co-authored-by: vinitava07 <vinitava07@gmail.com> Co-authored-by: lcsjhonyyy <lcsjhonyyy@gmail.com> Co-authored-by: AlexandreCJ <acjurka@sga.pucminas.br>

---
+++ gpt_admin_panel/lib/ui/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static get darkBG => const Color(0xFF363740);
+  static get lightBG => const Color(0xFFFFFFFF);
+  static get primary => const Color(0xFFFF7B21);
+}
