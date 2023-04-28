ab8db0e739c891964f1f2861d5e40ee4f620b6cb
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2023-04-28 10:35:01 -0300

    Creating questions getter

--- gpt/src/main/java/com/powerchat/gpt/controller/QuestionController.java
+++ gpt/src/main/java/com/powerchat/gpt/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.powerchat.gpt.controller;
 
+import com.powerchat.gpt.Services.QuestionService;
 import com.powerchat.gpt.dao.QuestionDAO;
 import com.powerchat.gpt.model.Question;
 import org.springframework.http.HttpStatus;
@@ -13,10 +14,12 @@ import java.util.List;
 public class QuestionController {
 
     @GetMapping("/question")
-    public ResponseEntity<String> handleHealthCheck() {
+    public ResponseEntity<String> handleHealthCheck() throws Exception {
         QuestionDAO questionDAO = new QuestionDAO();
         List<Question> questionList = questionDAO.getAll();
-
+        QuestionService questionService = new QuestionService();
+        questionService.parseJson(questionList);
+        String json = questionService.getQuestionServiceJson();
         return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 }
