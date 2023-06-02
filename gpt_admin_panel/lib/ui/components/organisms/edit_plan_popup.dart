7e1c783b64c389e1cdbf7a87627fc49516aa1373
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2023-06-01 22:11:50 -0300

    Setting height for container

--- gpt_admin_panel/lib/ui/components/organisms/edit_plan_popup.dart
+++ gpt_admin_panel/lib/ui/components/organisms/edit_plan_popup.dart
@@ -12,32 +12,35 @@ class EditPlanPopup extends StatelessWidget {
   Widget build(BuildContext context) {
     final textController = TextEditingController();
     return AlertDialog(
-      content: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Spacer(),
-          Text(
-            "Editar limite mensal de perguntas",
-            style: Theme.of(context).textTheme.headlineSmall,
-          ),
-          Text("plano: $planName"),
-          const Spacer(),
-          TextField(
-            controller: textController,
-          ),
-          const Spacer(),
-          MaterialButton(
-            onPressed: () {
-              final newValue = textController.text;
-              if (newValue.isNotEmpty) {
-                onTap(newValue);
-              }
-            },
-            color: AppColors.primary,
-            child: const Text('Salvar'),
-          ),
-          const Spacer(),
-        ],
+      content: SizedBox(
+        height: 440,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Text(
+              "Editar limite mensal de perguntas",
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            Text("plano: $planName"),
+            const Spacer(),
+            TextField(
+              controller: textController,
+            ),
+            const Spacer(),
+            MaterialButton(
+              onPressed: () {
+                final newValue = textController.text;
+                if (newValue.isNotEmpty) {
+                  onTap(newValue);
+                }
+              },
+              color: AppColors.primary,
+              child: const Text('Salvar'),
+            ),
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }
